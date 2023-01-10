1d3054d4ba78c259e118660e724e5753bff808d7
GF <dewy.i@kakaocorp.com>
2023-01-10 22:11:36 +0900

    [MATCHMAKER-19] 폼 컴포넌트 작성 (#1)
    * feat: use clsx
    
    * feat: edit lint rule
    
    * feat: edit Form
    
    * feat: edit FormInput
    
    * feat: edit SubmitButton

---
+++ src/components/form/index.tsx
@@ -0,0 +1,28 @@
+import {FC, FormEventHandler, ReactNode, useCallback} from 'react';
+
+export type OnSubmitHandler = (formData: Record<string, unknown>) => void;
+
+interface FormProps {
+  children: ReactNode;
+  onSubmit: OnSubmitHandler;
+}
+
+const Form: FC<FormProps> = ({
+  children,
+  onSubmit,
+}) => {
+  const submitHandler: FormEventHandler<HTMLFormElement> = useCallback(e => {
+    e.preventDefault();
+    const form = e.currentTarget as HTMLFormElement;
+    const formData = new FormData(form);
+    onSubmit(Object.fromEntries(formData.entries()));
+  }, []);
+
+  return (
+    <form onSubmit={submitHandler}>
+      {children}
+    </form>
+  );
+};
+
+export default Form;
