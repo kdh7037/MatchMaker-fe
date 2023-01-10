7d0d872fde9b870fdc7e8aee1f2d14ff75c4edc3
dewy.i <dewy.i@kakaocorp.com>
2023-01-10 22:13:41 +0900

    feat: 로그인 페이지 마크업 초안 작성

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,6 +1,33 @@
+import Form, {OnSubmitHandler} from '@components/form';
+import FormInput from '@components/form/input';
+import SubmitButton from '@components/form/submit';
+
 const LoginPage = () => {
+  const submitHandler: OnSubmitHandler = formData => {
+    console.log(formData);
+  };
+
   return (
-    <p>Login Page</p>
+    <section className="container p-4 m-auto">
+      <h1 className="mb-8 text-3xl">
+        로그인 페이지
+      </h1>
+      <Form onSubmit={submitHandler}>
+        <FormInput
+          label="아이디"
+          type="text"
+          name="id"
+          placeholder="아이디를 입력하세요."
+        />
+        <FormInput
+          label="비밀번호"
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력하세요."
+        />
+        <SubmitButton>로그인</SubmitButton>
+      </Form>
+    </section>
   );
 };
 
