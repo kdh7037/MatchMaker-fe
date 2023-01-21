ecc43dafbff87f911331d7ad2924aebb638f2f46
dewy.i <dewy.i@kakaocorp.com>
2023-01-21 21:21:43 +0900

    feat: add buttons to popup

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -1,15 +1,26 @@
+import clsx from 'clsx';
 import usePopup from './hooks/usePopup';
 
 const Popup = () => {
   const {popup} = usePopup();
 
-  return popup ? (
-    <div className="fixed top-0 left-0 flex w-screen h-screen">
-      <div className="p-4 m-auto">
-        {popup.content}
-      </div>
+  return (
+    <div
+      className={clsx(
+        'fixed top-0 left-0 flex w-screen h-screen bg-opacity-10 transition-[background-color]',
+        popup ? 'bg-black' : 'bg-transparent pointer-events-none'
+      )}
+    >
+      {!!popup && (
+        <div className="p-4 m-auto bg-white rounded shadow min-w-[25vw] pointer-events-auto">
+          {popup.content}
+          <div className="flex justify-start mt-8">
+            {popup.buttons}
+          </div>
+        </div>
+      )}
     </div>
-  ) : null;
+  );
 };
 
 export default Popup;
