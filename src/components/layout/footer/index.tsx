1f119b617713177892f5425e12c6b407f468fd25
dewy.i <dewy.i@kakaocorp.com>
2023-01-31 23:42:02 +0900

    feat: edit Footer

---
+++ src/components/layout/footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="w-full mt-auto bg-gray-100">
+      <div className="container p-4 mx-auto text-sm text-gray-400">
+        Match Maker
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
