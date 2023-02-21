1c09e64596c6f12c5554464d0cf694f8b4fdf0c7
ATRON3273 <adgjl16430@gmail.com>
2023-02-21 23:46:50 +0900

    feat: 마이 페이지 내 nickname, email input 입력 요구 메세지 State 추가

--- src/pages/my/index.page.tsx
+++ src/pages/my/index.page.tsx
@@ -2,10 +2,30 @@ import Form, {OnSubmitHandler} from '@components/form';
 import FormInput from '@components/form/input';
 import SubmitButton from '@components/form/submit';
 import Button from '@components/button';
+import { useState } from 'react';
 
 const MyPage = () => {
   const submitHandler: OnSubmitHandler = formData => {
     console.log(formData);
+  };
+
+  const [isNicknameFilled, setIsNicknameFilled] = useState(true);
+  const [isEmailFilled, setIsEmailFilled] = useState(true);
+
+  const requestWrite = (e) => {
+    if (e.target.name === "nickname") {
+      if (e.target.value.length > 0) {
+        setIsNicknameFilled(true);
+      } else {
+        setIsNicknameFilled(false);
+      }
+    } else if (e.target.name === "email") {
+      if (e.target.value.length > 0) {
+        setIsEmailFilled(true);
+      } else {
+        setIsEmailFilled(false);
+      }
+    }
   };
 
   return (
@@ -13,6 +33,7 @@ const MyPage = () => {
       <h1 className="mb-8 text-3xl">
         마이 페이지
       </h1>
+
       <Form onSubmit={submitHandler}>
         <FormInput
           label="아이디"
@@ -26,13 +47,17 @@ const MyPage = () => {
           type="input"
           name="nickname"
           placeholder="변경할 닉네임을 입력하세요."
+          onChange={requestWrite}
         />
+        <p>{!isNicknameFilled ? "닉네임을 입력해주세요." : null}</p>
         <FormInput
           label="이메일"
           type="email"
           name="email"
           placeholder="변경할 이메일을 입력하세요."
+          onChange={requestWrite}
         />
+        <p>{!isEmailFilled ? "이메일을 입력해주세요." : null}</p>
         <SubmitButton>수정</SubmitButton>
         <Button>돌아가기</Button>
       </Form>
