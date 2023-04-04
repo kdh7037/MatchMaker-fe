5a99fb97238522c173c5c066e1c5046071a086d9
ATRON3273 <adgjl16430@gmail.com>
2023-04-04 22:29:17 +0900

    feat: GNB 메뉴, 로그인/아웃 버튼 추가

--- src/components/layout/gnb/index.tsx
+++ src/components/layout/gnb/index.tsx
@@ -1,7 +1,63 @@
+import Button from '@components/button';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import Profile from './Profile';
 
 const GNB = () => {
+  const router = useRouter();
+
+  const [hasSession, setHasSession] = useState(true);
+
+  const redirectToLogin = () => {
+    router.push('/login');
+  };
+
+  const redirectToLogout = () => {
+    setHasSession(false);
+  };
+
+  const redirectToSignup = () => {
+    router.push('/signup');
+  };
+
+  const redirectToMatchList = () => {
+    router.push('/match');
+  };
+
+  const redirectToDiaryList = () => {
+    router.push('/diary');
+  };
+
+  const loginButton = (
+    <>
+      <Button
+        name="login"
+        onClick={redirectToLogin}
+      >
+        로그인
+      </Button>
+      <Button
+        name="signup"
+        onClick={redirectToSignup}
+      >
+        회원가입
+      </Button>
+    </>
+  );
+
+  const logoutButton = (
+    <>
+      <Profile />
+      <Button
+        name="logout"
+        onClick={redirectToLogout}
+      >
+        로그아웃
+      </Button>
+    </>
+  );
+
   return (
     <nav className="w-full text-white bg-blue-600 shadow">
       <div className="container flex items-center p-4 mx-auto">
@@ -10,7 +66,10 @@ const GNB = () => {
             Match Maker
           </Link>
         </h1>
-        <Profile />
+        <Button onClick={redirectToMatchList}>매칭</Button>
+        <Button onClick={redirectToDiaryList}>다이어리</Button>
+
+        {hasSession ? logoutButton : loginButton}
       </div>
     </nav>
   );
