68eb7ef2178ca64717121b602b921e0d6e80bfef
GF <gunhoflash@gmail.com>
2023-04-29 20:19:37 +0900

    [MATCHMAKER-19, MATCHMAKER-22] 로그인, 회원가입 작성 (#12)
    * feat: edit login page
    
    * feat: edit signup page
    
    * chore: generate styles
    
    ---------
    
    Co-authored-by: dewy.i <dewy.i@kakaocorp.com>

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,10 +1,26 @@
 import Form, {OnSubmitHandler} from '@components/form';
 import FormInput from '@components/form/input';
 import SubmitButton from '@components/form/submit';
+import {useCallback, useState} from 'react';
 
 const LoginPage = () => {
-  const submitHandler: OnSubmitHandler = formData => {
+  const [statusText, setStatusText] = useState('');
+  const resetStatusText = useCallback(() => setStatusText(''), []);
+
+  const submitHandler: OnSubmitHandler = async formData => {
     console.log(formData);
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    if (!response.ok) {
+      return setStatusText('로그인에 실패했습니다.');
+    }
+    const result: {result: boolean} = await response.json();
+    setStatusText(`로그인에 ${result.result ? '성공' : '실패'}했습니다.`);
   };
 
   return (
@@ -16,15 +32,22 @@ const LoginPage = () => {
         <FormInput
           label="아이디"
           type="text"
-          name="id"
+          name="userId"
+          onChange={resetStatusText}
           placeholder="아이디를 입력하세요."
         />
         <FormInput
           label="비밀번호"
           type="password"
           name="password"
+          onChange={resetStatusText}
           placeholder="비밀번호를 입력하세요."
         />
+        {!!statusText && (
+          <p className="mb-2 -my-2 text-sm text-red-600">
+            {statusText}
+          </p>
+        )}
         <SubmitButton>로그인</SubmitButton>
       </Form>
     </>
