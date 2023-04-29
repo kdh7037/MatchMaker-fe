f7540ab331cc842b7a7b02a8714837165c79716d
GF <gunhoflash@gmail.com>
2023-04-29 23:12:38 +0900

    chore: env, api (#11)
    * chore: edit .gitignore
    
    * chore: edit next.config.js
    
    * chore: install dotenv
    
    * chore: delete unnecessary
    
    * chore: set default api host
    
    ---------
    
    Co-authored-by: dewy.i <dewy.i@kakaocorp.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const API_HOST = process.env.API_HOST || 'http://localhost';
+const {parsed: parsedEnv} = dotenv.config({path: `env/${NODE_ENV}.env`});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: parsedEnv,
   images: {
     domains: [],
   },
   pageExtensions: ['page.tsx'],
   reactStrictMode: true,
+  rewrites: async () => {
+    return [{
+      source: '/api/:slug',
+      destination: `${API_HOST}/:slug`,
+    }];
+  },
 };
 
 module.exports = nextConfig;
